d77aaa92cc7c2f2b0710d2ec192b39345f4eb33a
tpf <tianpengfeiwork@sina.com>
2017-06-16 10:28:59 +0800

    初始导入！

---
+++ NH/lanban_nh_core/src/main/java/com/sixunhuan/nh/lanban_nh_core/App.java
@@ -0,0 +1,13 @@
+package com.sixunhuan.nh.lanban_nh_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ NH/lanbang_nh_dao/src/main/java/com/sixunhuan/nh/lanbang_nh_dao/App.java
@@ -0,0 +1,13 @@
+package com.sixunhuan.nh.lanbang_nh_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
